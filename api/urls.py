9656d6ab363e5f06ea2fe48a3350a169a519bc71
Shiela <shiela@bai.ph>
2021-12-21 16:06:56 +0800

    feat: add test view

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 
-from api.views.defaults_view import SetupDefaultsView 
+from api.views.defaults_view import SetupDefaultsView
+from api.views.views import TestView
 # from . import views
 
 
 #/api/method
 urlpatterns = [
+    # test 
+    path('test', TestView.as_view(), name='test_view'),
+    
     path('setup', SetupDefaultsView.as_view(), name='setups_default'),
     path('setup/<company_code>', SetupDefaultsView.as_view(), name='get_setups_default'),
+    
 ]
--- api/views/views.py
+++ api/views/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from os import stat
+from rest_framework import status
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class TestView(APIView):
+    def get(self, request, format=None):
+        return Response("test rest api get", status.HTTP_200_OK)
+
+    def post(self, request, format=None):
+        return Response(request.data, status.HTTP_200_OK)
+
+
 
 
 def welcome(request):
