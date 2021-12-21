ba6f2d88da33a9fc6074b940660f24d7d3da3b3a
Shiela <shiela@bai.ph>
2021-12-21 15:02:29 +0800

    feat: add debug on env

--- hexa/settings.py
+++ hexa/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.getenv('SECRET_KEY'))
 
 ALLOWED_HOSTS = ['127.0.0.1']
 ALLOWED_HOSTS.append(str(os.getenv('HOST')))
@@ -140,10 +140,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-
+if DEBUG == True:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
