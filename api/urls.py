2273024f869bc03551b3f2c014c9759dee26fb74
Shiela <shiela@bai.ph>
2022-01-05 22:57:44 +0800

    feat: add views for other stock module models

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,7 @@ from api.views.company_view import CompanyView
 from api.views.accounting_period_view import AccountingPeriodView
 from api.views.chartofaccounts_view import ChartOfAccountsView
 from api.views.views import TestView, TestViewCSV
-from api.views.stock_module_view import ItemCategoryView
+from api.views.stock_module_view import *
 # from . import views
 
 
@@ -15,13 +15,19 @@ urlpatterns = [
     path('test', TestView.as_view(), name='test_view'),
     path('test_csv', TestViewCSV.as_view(), name='test_view_csv'),
     
+    #settings
     path('setup', SetupDefaultsView.as_view(), name='setups_default'),
     path('setup/<company_code>', SetupDefaultsView.as_view(), name='get_setups_default'),
-
     path('company/<company_code>', CompanyView.as_view(), name='company_view'),
     path('accounting_period_list/<company_code>', AccountingPeriodView.as_view(), name='accounting_period_view'),
     path('coa_list/<company_code>', ChartOfAccountsView.as_view(), name='coa_view'),
     
     # stock module urls
     path('stock/item_category', ItemCategoryView.as_view(), name='item_category_view'),
+    path('stock/item_category/brand', ItemCatBrandView.as_view(), name='item_brand_view'),
+    path('stock/item_category/department', ItemCatDepartmentView.as_view(), name='item_dept_view'),
+    path('stock/item_category/form', ItemCatFormView.as_view(), name='item_form_view'),
+    path('stock/item_category/manufacturer', ItemCatManufacturerView.as_view(), name='item_manufacturer_view'),
+    path('stock/item_category/section', ItemCatSectionView.as_view(), name='item_section_view'),
+    path('stock/item_category/size', ItemCatSizeView.as_view(), name='item_size_view'),
 ]
--- api/views/stock_module_view.py
+++ api/views/stock_module_view.py
@@ -8,7 +8,9 @@ from rest_framework.views import APIView
 # models
 from api.models import ItemCategory, ItemCatBrand, ItemCatDepartment, ItemCatForm, ItemCatManufacturer, ItemCatSection, ItemCatSize
 # serializers 
-from api.serializers import ItemCategorySerializer, ItemCatBrandSerializer, ItemCatDepartmentSerializer, ItemCatFormSerializer, ItemCatManufacturerSerializer, ItemCatSectionSerializer, ItemCatSizeSerializer
+from api.serializers.stock_module_serializer import *
+
+
 
 class ItemCategoryView(APIView):
     
@@ -30,6 +32,177 @@ class ItemCategoryView(APIView):
 
     def post(self, request):
         item_cat_srlzr = ItemCategorySerializer(data=request.data)
+        if item_cat_srlzr.is_valid():
+            item_cat_srlzr.save()
+
+            return Response(item_cat_srlzr.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(item_cat_srlzr.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class ItemCatBrandView(APIView):
+    
+    def get(self, request):
+        try:
+            id = request.GET.get('id', None)
+            itemcats = ItemCatBrand.objects.all()
+            itemcatsserializer = ItemCatBrandSerializer(itemcats, many=True)
+
+            if id != None:
+                itemcats = ItemCatBrand.objects.get(id=id)
+                itemcatsserializer = ItemCatBrandSerializer(itemcats)
+            
+            data = itemcatsserializer.data
+            return Response(data, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response(str(e), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+    def post(self, request):
+        item_cat_srlzr = ItemCatBrandSerializer(data=request.data)
+        if item_cat_srlzr.is_valid():
+            item_cat_srlzr.save()
+
+            return Response(item_cat_srlzr.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(item_cat_srlzr.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class ItemCatDepartmentView(APIView):
+    
+    def get(self, request):
+        try:
+            id = request.GET.get('id', None)
+            itemcats = ItemCatDepartment.objects.all()
+            itemcatsserializer = ItemCatDepartmentSerializer(itemcats, many=True)
+
+            if id != None:
+                itemcats = ItemCatDepartment.objects.get(id=id)
+                itemcatsserializer = ItemCatDepartmentSerializer(itemcats)
+            
+            data = itemcatsserializer.data
+            return Response(data, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response(str(e), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+    def post(self, request):
+        item_cat_srlzr = ItemCatDepartmentSerializer(data=request.data)
+        if item_cat_srlzr.is_valid():
+            item_cat_srlzr.save()
+
+            return Response(item_cat_srlzr.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(item_cat_srlzr.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ItemCatFormView(APIView):
+    
+    def get(self, request):
+        try:
+            id = request.GET.get('id', None)
+            itemcats = ItemCatForm.objects.all()
+            itemcatsserializer = ItemCatFormSerializer(itemcats, many=True)
+
+            if id != None:
+                itemcats = ItemCatForm.objects.get(id=id)
+                itemcatsserializer = ItemCatFormSerializer(itemcats)
+            
+            data = itemcatsserializer.data
+            return Response(data, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response(str(e), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+    def post(self, request):
+        item_cat_srlzr = ItemCatFormSerializer(data=request.data)
+        if item_cat_srlzr.is_valid():
+            item_cat_srlzr.save()
+
+            return Response(item_cat_srlzr.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(item_cat_srlzr.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ItemCatManufacturerView(APIView):
+    
+    def get(self, request):
+        try:
+            id = request.GET.get('id', None)
+            itemcats = ItemCatManufacturer.objects.all()
+            itemcatsserializer = ItemCatManufacturerSerializer(itemcats, many=True)
+
+            if id != None:
+                itemcats = ItemCatManufacturer.objects.get(id=id)
+                itemcatsserializer = ItemCatManufacturerSerializer(itemcats)
+            
+            data = itemcatsserializer.data
+            return Response(data, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response(str(e), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+    def post(self, request):
+        item_cat_srlzr = ItemCatManufacturerSerializer(data=request.data)
+        if item_cat_srlzr.is_valid():
+            item_cat_srlzr.save()
+
+            return Response(item_cat_srlzr.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(item_cat_srlzr.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class ItemCatSectionView(APIView):
+    
+    def get(self, request):
+        try:
+            id = request.GET.get('id', None)
+            itemcats = ItemCatSection.objects.all()
+            itemcatsserializer = ItemCatSectionSerializer(itemcats, many=True)
+
+            if id != None:
+                itemcats = ItemCatSection.objects.get(id=id)
+                itemcatsserializer = ItemCatSectionSerializer(itemcats)
+            
+            data = itemcatsserializer.data
+            return Response(data, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response(str(e), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+    def post(self, request):
+        item_cat_srlzr = ItemCatSectionSerializer(data=request.data)
+        if item_cat_srlzr.is_valid():
+            item_cat_srlzr.save()
+
+            return Response(item_cat_srlzr.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(item_cat_srlzr.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ItemCatSizeView(APIView):
+    
+    def get(self, request):
+        try:
+            id = request.GET.get('id', None)
+            itemcats = ItemCatSize.objects.all()
+            itemcatsserializer = ItemCatSizeSerializer(itemcats, many=True)
+
+            if id != None:
+                itemcats = ItemCatSize.objects.get(id=id)
+                itemcatsserializer = ItemCatSizeSerializer(itemcats)
+            
+            data = itemcatsserializer.data
+            return Response(data, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response(str(e), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+    def post(self, request):
+        item_cat_srlzr = ItemCatSizeSerializer(data=request.data)
         if item_cat_srlzr.is_valid():
             item_cat_srlzr.save()
 
